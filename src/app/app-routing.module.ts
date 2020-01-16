ec133f43d832933847a2e28ea03a6fe7ec04e159
Andres Lopez <andgvo@gmail.com>
2020-01-16 01:28:50 -0600

    Revert "Se quito la restricción de login para usar en produccion"
    This reverts commit e9a8070c2f683169958c2c3f887bb370c61777db.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    useHash: true,
+    //useHash: true,
     initialNavigation: 'enabled',
     scrollPositionRestoration: 'enabled',
     anchorScrolling: 'enabled',
