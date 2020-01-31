786d3c7cd4a151ed36f86814e2c95d1ffc18628f
Andres Lopez <andgvo@gmail.com>
2020-01-31 08:27:30 -0600

    fix(app-routing): Se agrego el uso de hash en el routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    //useHash: true,
+    useHash: true,
     initialNavigation: 'enabled',
     scrollPositionRestoration: 'enabled',
     anchorScrolling: 'enabled',
