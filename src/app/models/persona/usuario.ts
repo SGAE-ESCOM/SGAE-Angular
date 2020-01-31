9fb64427f9bf412199ecacac5e59c92aef41c60e
Andres Lopez <andgvo@gmail.com>
2020-01-31 09:19:39 -0600

    add(Documentacion-Validar): Se agrego la tabla para visualizar los usuarios disponibles

---
+++ src/app/models/persona/usuario.ts
@@ -0,0 +1,15 @@
+export interface Roles {
+    aspirante?: boolean;
+    admin?: boolean;
+    root?: boolean;
+}
+
+export interface UsuarioInterface {
+    id?: string;
+    nombres?: string;
+    apellidos?: string;
+    email?: string;
+    password?: string;
+    photoUrl?: string;
+    roles: Roles;
+}
