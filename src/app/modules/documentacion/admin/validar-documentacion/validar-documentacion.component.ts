bb34fae13b94b3332bba0787284f2b8aab86a6e0
Andres Lopez <andgvo@gmail.com>
2020-02-07 12:18:28 -0600

    revert 7-Feb-20

--- src/app/modules/documentacion/admin/validar-documentacion/validar-documentacion.component.ts
+++ src/app/modules/documentacion/admin/validar-documentacion/validar-documentacion.component.ts
@@ -1,64 +1,19 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreadcrumbComponent } from '@shared/breadcrumb/breadcrumb.component';
 import { BC_VALIDAR_DOCUMENTACION } from '@shared/routing-list/ListLinks';
-import { UsuarioInterface } from '@models/persona/usuario';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-validar-documentacion',
   templateUrl: './validar-documentacion.component.html',
   styleUrls: ['./validar-documentacion.component.scss']
 })
-export class ValidarDocumentacionComponent implements OnInit, AfterViewInit{
-
-  //Variables para las tablas
-  displayedColumns: string[] = ['nombres', 'apellidos', 'estado', 'acciones'];
-  usuarios: MatTableDataSource<any> = new MatTableDataSource();;
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
-
-  //Varibale DEBUG
-  listaUsuarios: any[] = [ //DEBUG
-    { "id":"1", "nombres": "Gustavo Andres", "apellidos": "Lopez Sanchez", "estado": "SIN_REVISION"},
-    { "id":"2", "nombres": "Aiko Dallane", "apellidos": "López Rivera", "estado": "SIN_REVISION"},
-    { "id":"3", "nombres": "Edgar", "apellidos": "Flores Altamirano", "estado": "SIN_REVISION"},
-    { "id":"4", "nombres": "Christian Andres", "apellidos": "Cervantes Moreno", "estado": "SIN_REVISION"},
-  ];
-  filtros: string[] = [
-    "Sin revision",
-    "Pendiente de correccion",
-    "Validado"
-  ]
+export class ValidarDocumentacionComponent implements OnInit {
 
   constructor() { 
     BreadcrumbComponent.update(BC_VALIDAR_DOCUMENTACION);
   }
 
   ngOnInit() {
-    //this._ads.getDocumentos().subscribe( (documentos:TipoDato[]) => this.documentos.data = documentos ); //PRODUCCION
-    this.usuarios.data = this.listaUsuarios; // DEBUG
-  }
-
-  ngAfterViewInit() {
-    this.updateTablaUsuarios();
-  }
-
-  private updateTablaUsuarios(): void {
-    this.usuarios.paginator = this.paginator;
-    this.usuarios.sort = this.sort;
-  }
-
-  //Eventos
-  onChangeFiltroUsuario(filtro){
-    alert(filtro);
-  }
-
-  buscarUsuario(filterValue: string) {
-    this.usuarios.filter = filterValue.trim().toLowerCase();
-    if (this.usuarios.paginator) {
-      this.usuarios.paginator.firstPage();
-    }
   }
 
 }
