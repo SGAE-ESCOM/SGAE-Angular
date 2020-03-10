6428b477fc4fc48e840b74f7a9e86335357a8429
EdgrFA <foae_97@hotmail.com>
2020-03-09 18:27:10 -0600

    fix(usuarios): Problemas con importaciones

--- src/app/modules/usuarios/usuarios.module.ts
+++ src/app/modules/usuarios/usuarios.module.ts
@@ -5,12 +5,10 @@ import { UsuariosRoutingModule } from './usuarios-routing.module';
 /* Components */
 import { MainUsuariosComponent } from './main-usuarios/main-usuarios.component';
 import { CardsModule } from '@shared/cards/cards.module';
-import { GestionAspirantesComponent } from './components/gestion-aspirantes/gestion-aspirantes.component';
-import { GestionAdmonComponent } from './components/gestion-admon/gestion-admon.component';
 
 
 @NgModule({
-  declarations: [MainUsuariosComponent, GestionAspirantesComponent, GestionAdmonComponent],
+  declarations: [MainUsuariosComponent],
   imports: [
     CommonModule,
     UsuariosRoutingModule,
