0ca8a6db95af53610704b469c2720f38d79d4fec
Andres <andgvo@gamil.com>
2020-10-19 21:43:25 -0500

    update(Grupos): Se finaliza el modulo de creacion de grupos

--- src/app/modules/evaluacion/admin/grupos/form-grupos/form-grupos.component.ts
+++ src/app/modules/evaluacion/admin/grupos/form-grupos/form-grupos.component.ts
@@ -4,6 +4,7 @@ import { Grupo } from '@models/evaluacion/Grupo';
 import { GruposService } from '@services/evaluacion/grupos.service';
 import { ALPHANUMERICO_CON_ESPACIOS } from '@shared/utils/validators/regex';
 import { ToastrService } from 'ngx-toastr';
+import { Subject } from 'rxjs';
 
 
 @Component({
@@ -11,59 +12,69 @@ import { ToastrService } from 'ngx-toastr';
   templateUrl: './form-grupos.component.html',
   styleUrls: ['./form-grupos.component.scss']
 })
-export class FormGruposComponent implements OnInit, OnChanges{
+export class FormGruposComponent implements OnInit, OnChanges {
 
-  @Input() opc:string;
+  @Input() opc: string;
   @Input() grupo: Grupo;
   @Output() cerrar: EventEmitter<boolean> = new EventEmitter<boolean>();
   @Output() accion: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   fgGrupo: FormGroup;
+  isForm: Subject<Boolean> = new Subject<Boolean>();
 
-  constructor(private fb:FormBuilder, private _grupos:GruposService, private _toastr: ToastrService) { }
- 
+  constructor(private fb: FormBuilder, private _grupos: GruposService, private _toastr: ToastrService) { }
+
   ngOnInit(): void {
-    this.fgGrupo = this.fb.group({
-      nombre: ['', [Validators.required, Validators.maxLength(50), Validators.pattern(ALPHANUMERICO_CON_ESPACIOS)]]
-    });
+    this.initForm();
+    this.isForm.next(true);
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if(changes.grupo && this.grupo != null ){
-      console.log(changes);
-      console.log(this.fgGrupo)
-      this.fgGrupo.get('nombre').setValue(this.grupo.nombre);
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.grupo && this.grupo != null) {
+      this.isForm.subscribe(result => {
+        if (result)
+          this.fgGrupo.get('nombre').setValue(this.grupo.nombre);
+      });
+
     }
   }
 
+  initForm(): Promise<Boolean> {
+    this.fgGrupo = this.fb.group({
+      nombre: ['', [Validators.required, Validators.maxLength(50), Validators.pattern(ALPHANUMERICO_CON_ESPACIOS)]]
+    });
+    return new Promise<Boolean>(resolve => { return true });
+  }
 
-  save(form:FormGroup){
-    if( form.valid){
-      this._grupos.save(form.value).then( caso => { 
+  save(form: FormGroup) {
+    if (form.valid) {
+      this._grupos.save(form.value).then(caso => {
         this._toastr.success("Agregado correctamente");
         this.accion.emit(true);
       }, err => {
         this._toastr.error("Ha ocurrido un error");
       });
-    }else{
+    } else {
       this._toastr.error("Debes agregar un nombre");
     }
   }
 
-  update(form: FormGroup){
-    if( form.valid){
-      this._grupos.update(form.value).then( caso => { 
+  update(form: FormGroup) {
+    if (form.valid) {
+      let grupo:Grupo = form.value;
+      grupo.id = this.grupo.id;
+      this._grupos.update(grupo).then(caso => {
         this._toastr.success("Actualizado correctamente");
         this.accion.emit(true);
       }, err => {
         this._toastr.error("Ha ocurrido un error");
       });
-    }else{
+    } else {
       this._toastr.error("Debes agregar un nombre");
     }
   }
-  
-  cerrarModal(){
+
+  cerrarModal() {
     this.cerrar.emit(true);
   }
 
