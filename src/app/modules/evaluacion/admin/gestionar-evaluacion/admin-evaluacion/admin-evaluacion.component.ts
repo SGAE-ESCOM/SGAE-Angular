1acde88a8fdbd5d126a073c88a121db8ecddaee5
Andres <andgvo@gamil.com>
2020-11-13 17:49:10 -0600

    update(Evaluacion): Se agregaron name y id a elementos para la automatizacion

--- src/app/modules/evaluacion/admin/gestionar-evaluacion/admin-evaluacion/admin-evaluacion.component.ts
+++ src/app/modules/evaluacion/admin/gestionar-evaluacion/admin-evaluacion/admin-evaluacion.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { BreadcrumbComponent } from '@components/breadcrumb/breadcrumb.component';
+import { Tabla } from '@models/utils/Tabla';
+import { BC_ADMIN_EVALUACION } from '@shared/routing-list/ListLinks';
+import { fadeInRight } from '@shared/utils/animations/router.animations';
 
 @Component({
   selector: 'app-admin-evaluacion',
   templateUrl: './admin-evaluacion.component.html',
-  styleUrls: ['./admin-evaluacion.component.scss']
+  styleUrls: ['./admin-evaluacion.component.scss'],
+  animations: [fadeInRight()]
 })
 export class AdminEvaluacionComponent implements OnInit {
 
-  constructor() { }
+  columnasEvaluacion: Tabla[] = [
+    {encabezado:'Nombre', json:'nombre'}, {encabezado:'Grupos', json:'grupos'}, {encabezado:'Temas', json:'temas'}, {encabezado:'Acciones', json: 'acciones'}
+  ];
+  constructor() {
+    BreadcrumbComponent.update(BC_ADMIN_EVALUACION)
+  }
 
   ngOnInit(): void {
   }
