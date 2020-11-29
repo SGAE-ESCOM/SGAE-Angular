6a04aeccd6fbf9939caeb29c5cf246cc9748e06e
EdgrFA <foae_97@hotmail.com>
2020-11-29 04:11:37 -0600

    add(aspirantes-pagos): datos formato pago completo

--- src/app/modules/pagos/aspirante/pagos-aspirante.module.ts
+++ src/app/modules/pagos/aspirante/pagos-aspirante.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { PagosAspiranteRoutingModule } from './pagos-aspirante-routing.module';
 import { FormatoPagoComponent } from './formato-pago/formato-pago.component';
 import { EvidenciaPagoComponent } from './evidencia-pago/evidencia-pago.component';
+import { MatSelectModule } from '@angular/material/select';
 
 
 @NgModule({
   declarations: [FormatoPagoComponent, EvidenciaPagoComponent],
   imports: [
     CommonModule,
-    PagosAspiranteRoutingModule
+    PagosAspiranteRoutingModule,
+    MatSelectModule
   ]
 })
 export class PagosAspiranteModule { }
