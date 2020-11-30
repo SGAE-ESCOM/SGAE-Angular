3fabef2b14f280357a0e6899d0770bd3da05640b
EdgrFA <foae_97@hotmail.com>
2020-11-30 15:24:46 -0600

    add(aspirantes-pagos): drag and drop comprobante de pago

--- src/app/modules/pagos/aspirante/pagos-aspirante.module.ts
+++ src/app/modules/pagos/aspirante/pagos-aspirante.module.ts
@@ -7,16 +7,21 @@ import { EvidenciaPagoComponent } from './evidencia-pago/evidencia-pago.componen
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { AngularMaterialModule } from '@modules/template/angular-material.module';
+import { DndDirective } from './evidencia-pago/dnd.directive';
+import { ProgressComponent } from './evidencia-pago/progress/progress.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [FormatoPagoComponent, EvidenciaPagoComponent],
+  declarations: [FormatoPagoComponent, EvidenciaPagoComponent, DndDirective, ProgressComponent],
   imports: [
     CommonModule,
     PagosAspiranteRoutingModule,
     MatSelectModule,
     MatButtonModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    FormsModule
   ]
 })
 export class PagosAspiranteModule { }
