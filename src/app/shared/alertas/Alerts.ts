de88fb5c55158c19f0dcbf2cccb8ac668c055c26
EdgrFA <foae_97@hotmail.com>
2020-12-14 01:47:30 -0600

    update(alertas): se agrego alerta

--- src/app/shared/alertas/Alerts.ts
+++ src/app/shared/alertas/Alerts.ts
@@ -4,8 +4,9 @@ import * as ListLinks from '@shared/routing-list/ListLinks';
 export const ALERTAS = new Map();
 
 const NAME_EVIDENCIA_INVALIDA = "EVIDENCIA_INVALIDA";
-const EVIDENCIA_INVALIDA = new Alert(NAME_EVIDENCIA_INVALIDA, "Tu evidencia de pago ha sido rechazada", ListLinks.EVIDENCIA_PAGO, TipoAlert.ALERTA);
+const EVIDENCIA_INVALIDA = new Alert(NAME_EVIDENCIA_INVALIDA, "Tu evidencia de pago ha sido rechazada", ListLinks.EVIDENCIA_PAGO.url, TipoAlert.ALERTA);
 ALERTAS.set(NAME_EVIDENCIA_INVALIDA, EVIDENCIA_INVALIDA);
-// export const ALERTAS = {
-//     NAME_EVIDENCIA_INVALIDA : EVIDENCIA_INVALIDA
-// }
+
+const NAME_EVIDENCIA_CORRECTA = "EVIDENCIA_CORRECTA";
+const EVIDENCIA_CORRECTA = new Alert(NAME_EVIDENCIA_INVALIDA, "Tu evidencia de pago ha sido aprobada", ListLinks.EVIDENCIA_PAGO.url, TipoAlert.CONFIRMACION);
+ALERTAS.set(NAME_EVIDENCIA_CORRECTA, EVIDENCIA_CORRECTA);
