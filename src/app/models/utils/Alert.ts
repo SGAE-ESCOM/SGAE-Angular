d6dd69faaad5a44c4bff3136bed614cb7eb72f38
EdgrFA <foae_97@hotmail.com>
2020-12-31 17:33:21 -0600

    update(alertas): Ahora se borran las alertas de pagos al validar el pago, y las alertas tambien se pueden borrar al presionarlas

--- src/app/models/utils/Alert.ts
+++ src/app/models/utils/Alert.ts
@@ -10,11 +10,13 @@ export class Alert {
     mensaje: string;
     url: string;
     tipo: TipoAlert;
+    removerOnClick: boolean;
 
-    constructor(nombre: string, mensaje: string, url: string, tipo: TipoAlert){ 
+    constructor(nombre: string, mensaje: string, url: string, tipo: TipoAlert, removerOnClick: boolean = false){ 
         this.nombre = nombre;
         this.mensaje = mensaje;
         this.url = url;
         this.tipo = tipo;
+        this.removerOnClick = removerOnClick;
     }
 }
--- src/app/shared/alertas/Alerts.ts
+++ src/app/shared/alertas/Alerts.ts
@@ -3,16 +3,34 @@ import * as ListLinks from '@shared/routing-list/ListLinks';
 
 export const ALERTAS = new Map();
 
+//PAGOS
+
 const NAME_EVIDENCIA_INVALIDA = "EVIDENCIA_INVALIDA";
-export const EVIDENCIA_INVALIDA = new Alert(NAME_EVIDENCIA_INVALIDA, "Tu evidencia de pago ha sido rechazada.", ListLinks.EVIDENCIA_PAGO.url, TipoAlert.ALERTA);
+export const EVIDENCIA_INVALIDA = new Alert(NAME_EVIDENCIA_INVALIDA, "Evidencia de pago rechazada.", ListLinks.EVIDENCIA_PAGO.url, TipoAlert.ALERTA);
 ALERTAS.set(NAME_EVIDENCIA_INVALIDA, EVIDENCIA_INVALIDA);
 
 const NAME_EVIDENCIA_CORRECTA = "EVIDENCIA_CORRECTA";
-export const EVIDENCIA_CORRECTA = new Alert(NAME_EVIDENCIA_CORRECTA, "Tu evidencia de pago ha sido aprobada.", ListLinks.EVIDENCIA_PAGO.url, TipoAlert.CONFIRMACION);
+export const EVIDENCIA_CORRECTA = new Alert(NAME_EVIDENCIA_CORRECTA, "Evidencia de pago aprobada.", ListLinks.EVIDENCIA_PAGO.url, TipoAlert.CONFIRMACION, true);
 ALERTAS.set(NAME_EVIDENCIA_CORRECTA, EVIDENCIA_CORRECTA);
+
+export const ALERTAS_PAGOS = [EVIDENCIA_INVALIDA, EVIDENCIA_CORRECTA];
 
 export function getAlertas(names: Array<string>): Array<Alert> {
     let alerts : Array<Alert> = new Array();
     names.forEach(name => alerts.push(ALERTAS.get(name)));
     return alerts;
+}
+
+export function removerGrupoAlertas(alertas: Array<string>, grupoAlertas: Alert[]){
+    grupoAlertas.forEach(alerta => {
+        let index = alertas.indexOf(alerta.nombre, 0);
+        if (index > -1) alertas.splice(index, 1);
+    });
+    return alertas;
+}
+
+export function removerAlerta(alertas: Array<string>, alerta: Alert){
+    let index = alertas.indexOf(alerta.nombre, 0);
+    if (index > -1) alertas.splice(index, 1);
+    return alertas;
 }
